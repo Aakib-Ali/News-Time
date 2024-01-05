cbf315aad00fe75fb2ca60d884a2eb7839aa0fb2
Aakib-Ali <aakibali449@gmail.com>
2024-01-05 13:54:36 +0530

    Category

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+  render() {
+    return (
+      <div>
+        This is about us
+      </div>
+    )
+  }
+}
+
+export default About
+
--- src/components/News.js
+++ src/components/News.js
@@ -1,57 +1,69 @@
 import React, { Component } from 'react';
 import NewsItems from './NewsItems';
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 class News extends Component {
+    static defaultProps={
+        country: 'in',
+        pageSize: 8,
+        category: 'general'
+    }
+    static propTypes={
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
+    }
+
     constructor() {
         super();
         this.state = {
             articles: [],
             loading: false,
-            page:1
+            page: 1
         };
     }
-    
+
     async componentDidMount() {
-        console.log("cdm");
-        let url = `https://newsapi.org/v2/everything?q=tesla&from=2023-12-04&sortBy=publishedAt&apiKey=aa8657647fa6452ba3c85d56f5a2e5c0&page=1&pagesize=${this.props.pageSize}`;
-        this.setState({loading:true})
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=aa8657647fa6452ba3c85d56f5a2e5c0&page=1&pagesize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let data = await fetch(url);
+        let parsedata = await data.json();
+        this.setState({ articles: parsedata.articles, totalResult: parsedata.totalResults, loading: false });
+    }
+
+    handleNext = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=aa8657647fa6452ba3c85d56f5a2e5c0&page=${this.state.page + 1}&pagesize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedata = await data.json();
         console.log(parsedata);
-        this.setState({ articles: parsedata.articles, totalResult: parsedata.totalResult, loading:false });
-    }
-    
-    handleNext = async () => { 
-            let url = `https://newsapi.org/v2/everything?q=tesla&from=2023-12-04&sortBy=publishedAt&apiKey=aa8657647fa6452ba3c85d56f5a2e5c0&page=${this.state.page + 1}&pagesize=${this.props.pageSize}`;
-            this.setState({loading:true})
-            let data = await fetch(url);
-            let parsedata = await data.json();
-            console.log(parsedata);
-            this.setState({
-                articles: parsedata.articles,
-                page: this.state.page + 1,
-                loading:false
-            });
+        this.setState({
+            articles: parsedata.articles,
+            page: this.state.page + 1,
+            loading: false
+        });
     };
+
     handlePrevious = async () => {
         console.log("cdm");
-        let url = `https://newsapi.org/v2/everything?q=tesla&from=2023-12-04&sortBy=publishedAt&apiKey=aa8657647fa6452ba3c85d56f5a2e5c0&page=${this.state.page - 1}&pagesize=${this.props.pageSize}`;
-        this.setState({loading:true})
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=aa8657647fa6452ba3c85d56f5a2e5c0&page=${this.state.page - 1}&pagesize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedata = await data.json();
         console.log(parsedata);
         this.setState({
             articles: parsedata.articles,
             page: this.state.page - 1,
-            loading:false
+            loading: false
         });
     };
-    
+
     render() {
         return (
             <div className='container my-auto'>
                 <h1 className='text-center'>Top Headline</h1>
-                {this.state.loading && <Spinner/>}
+                {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => (
                         <div className="col-md-3 my-1" key={element.url}>
@@ -65,9 +77,9 @@ class News extends Component {
                     ))}
                 </div>
                 <div className="d-flex justify-content-between">
-                           <button disabled={this.state.page<=1} className="btn btn-sm btn-primary my-3" onClick={this.handlePrevious}>&larr; previous</button> 
-                           <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResult / 20)} className='btn btn-sm btn-primary my-3' onClick={this.handleNext}>next &rarr;</button>
-                        </div>
+                    <button disabled={this.state.page <= 1} className="btn btn-sm btn-primary my-3" onClick={this.handlePrevious}>&larr; previous</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResult / this.props.pageSize)} className='btn btn-sm btn-primary my-3' onClick={this.handleNext}>next &rarr;</button>
+                </div>
             </div>
         );
     }
